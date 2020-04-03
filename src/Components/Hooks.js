08c1932bdad459ef5a7e0564fbbcf0f01d2d7808
Raposhian <ryan.aposhian@gmail.com>
2020-04-03 15:11:43 -0600

    finished lecture

--- src/Components/Hooks.js
+++ src/Components/Hooks.js
@@ -1,30 +1,58 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class Hooks extends Component {
-    constructor(){
-        super();
-        this.state = {
-            count: 0,
-            name: ''
-        }
-    }
+// class Hooks extends Component {
+//     constructor(){
+//         super();
+//         this.state = {
+//             count: 0,
+//             name: ''
+//         }
+//     }
 
-    handleAdd(){
-    this.setState({ count: count + 1})
-}
+//     handleAdd(){
+//     this.setState({ count: count + 1})
+// }
 
-    handleInput(value){
-     this.setState({name: e.target.value})
-}
+//     handleInput(value){
+//      this.setState({name: e.target.value})
+// }
 
-    render(){
-        return(
-            <div>
-                {this.state.count}
-                 <input onChange={(e) => this.handleInput(e.target.value)}/>
-            </div>
-        )
-    }
+//     render(){
+//         return(
+//             <div>
+//                 {this.state.count}
+//                  <input onChange={(e) => this.handleInput(e.target.value)}/>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Hooks;
+
+import React, {useState, useEffect} from 'react';
+import NameDisplay from './NameDisplay';
+
+const Hooks = props => {
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
+    
+    useEffect(() => {
+        console.log('UseEffect simulated a componentDidMount')
+    }, []);
+
+    useEffect(()=> {
+        console.log('component did update')
+    }, [count, name])
+
+    return (
+        <>
+            <NameDisplay name={name}/>
+            <input value={name} onChange={e => setName(e.target.value)} />
+            <div>{count}</div>
+            <button onClick={()=> setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </>
+    )
 }
 
 export default Hooks;
